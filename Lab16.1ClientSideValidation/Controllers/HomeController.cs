d7811443ae534960d4605bbba11de0b9b79642bb
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-10-06 00:49:28 -0400

    Password validation working

---
+++ Lab16.1ClientSideValidation/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab16._1ClientSideValidation.Models;
+
+namespace Lab16._1ClientSideValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAccount(string FirstName, string LastName, string Password, string Email)
+        {
+            User user = new User() { FirstName = FirstName, LastName = LastName, Password = Password, Email = Email };
+
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
